3ac79b8759f4ed35f3a17c9305e7dbe80eaab59e
VarshithaJ07 <varshavarshithaj@gmail.com>
2026-03-29 13:17:31 +0530

    db.py updated

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,14 @@
 import mysql.connector
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="varsha",   
-        database="task_manager"
-    )
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="varsha",   # 🔴 change this
+            database="task_manager"
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print("Database connection error:", err)
+        return None
